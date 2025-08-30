181adfeb42d9c94dac6ab83da3aae561c575be4f
soumili duttta <soumilid05@gmail.com>
2025-08-31 01:13:26 +0530

    initial comment

---
+++ alter_table.js
@@ -0,0 +1,11 @@
+//Altering a Table
+
+//Add a Column
+ALTER TABLE users ADD COLUMN age INT;
+
+//Drop a Column
+ALTER TABLE users DROP COLUMN age;
+
+//Modify a Column Type
+ALTER TABLE users MODIFY COLUMN name VARCHAR(150);
+
