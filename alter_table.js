b76044d7ea5e919d18a1beb5f308453a606264b2
soumili duttta <soumilid05@gmail.com>
2025-08-31 01:24:50 +0530

    initial comment

--- alter_table.js
+++ alter_table.js
@@ -9,3 +9,10 @@ ALTER TABLE users DROP COLUMN age;
 //Modify a Column Type
 ALTER TABLE users MODIFY COLUMN name VARCHAR(150);
 
+//Move a Column to the First Position:-
+
+//To move a column (e.g., email ) to the first position:
+ALTER TABLE users MODIFY COLUMN email VARCHAR(100) FIRST;
+
+//To move a column after another column (e.g., move gender after name ):
+ALTER TABLE users MODIFY COLUMN gender ENUM('Male', 'Female', 'Other') AFTER name;
