ee9289ad64ca044c84b99d85a3e8e2395d0dbd26
soumili duttta <soumilid05@gmail.com>
2025-08-31 20:12:57 +0530

    initial comment

---
+++ insert_data.js
@@ -0,0 +1,8 @@
+//Insert Without Specifying Column Names (Full Row Insert)
+INSERT INTO USER VALUES(1,'SOUMILI','SOUMILI21@GMAIL.COM','FEMALE','21-08-2003','DEFAULT');
+//Insert Multiple Rows at Once
+
+INSERT INTO users (name, email, gender, date_of_birth) VALUES
+('Charlie', 'charlie@example.com', 'Other', '1988-02-17'),
+('David', 'david@example.com', 'Male', '2000-08-09'),
+('Eva', 'eva@example.com', 'Female', '1993-12-30');
